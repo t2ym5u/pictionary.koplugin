4d90d879d958098044e7e2bce5adc39dc2dd160b
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,4 @@
+return {
+    ["Draw words against the clock — team-based party game."] = { fr = "Dessinez des mots contre la montre — jeu de groupe en équipes." },
+    ["Pictionary Party"] = { fr = "Pictionary Party" },
+}
